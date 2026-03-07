db249306d8eccbdb57b3dc0a3b929dfbacc0d58b
Ben-Mast <benjaminmst@gmail.com>
2026-03-07 16:32:44 -0700

    changed ignored make, changed settings redirects

--- hikingProject/hikingProject/settings.py
+++ hikingProject/hikingProject/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "core.User"
-LOGIN_REDIRECT_URL = "index"
+LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
 LOGIN_URL = "login"
