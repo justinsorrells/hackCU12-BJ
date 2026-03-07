a756ec4447e3af8dbbf52a7f165eae0261af9f4b
Justin Sorrells <justinwsorrells@gmail.com>
2026-03-07 13:44:01 -0700

    added user model

--- hikingProject/core/models.py
+++ hikingProject/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    GENDER = [
+        ("F", "Female"),
+        ("M", "Male"),
+        ("N", "Non-binary"),
+    ]
+    EXPERIENCE_LEVEL = [
+        ("B", "Beginner"),
+        ("I", "Intermediate"),
+        ("E", "Experienced"),
+    ]
+    PACE = [
+        ("S", "Slow"), 
+        ("A", "Average"),
+        ("F", "Fast"),
+    ]
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)    
+    gender = models.CharField(max_length=1, choices=GENDER)
+    experience_level = models.CharField(max_length=1, choices=EXPERIENCE_LEVEL)
+    pace = models.CharField(max_length=1, choices=PACE)
+
+
