604f7082081c27fd6409d98451bf24efc9632b53
Justin Sorrells <justinwsorrells@gmail.com>
2026-03-07 15:58:00 -0700

    forced authentication

--- hikingProject/core/views.py
+++ hikingProject/core/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
 
 # Create your views here.
+@login_required
 def home(request):
     return HttpResponse("Hello, Ben!")
 
