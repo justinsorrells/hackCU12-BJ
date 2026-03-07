47c0aa666389c06057537abaf00c0504c0167847
Justin Sorrells <justinwsorrells@gmail.com>
2026-03-07 15:36:53 -0700

    added registration and login

--- hikingProject/hikingProject/settings.py
+++ hikingProject/hikingProject/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "core.User"
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "login"
+LOGIN_URL = "login"
